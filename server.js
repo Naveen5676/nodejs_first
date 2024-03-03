0cc0d563eabf5d7c9765df377e6bab278a21edb9
Naveen5676 <sample.first@yahoo.com>
2024-03-03 13:21:58 +0530

    Request / Response

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-    console.log(req)
-    console.log('naveen')
+    const url = req.url
+    if (url== '/home'){
+        res.write('<html><head><title>Home Page</title></head><body><h1> Welcome to Home Page</body></html>');
+        res.end();
+    }
+    if (url== '/about'){
+        res.write('<html><head><title> About Us</title></head><body><h1> Welcome to About Us page</body></html>');
+        res.end();
+    }
+    if (url== '/node'){
+        res.write('<html><head><title>Node Js </title></head><body><h1> Welcome to my Node Js project</body></html>');
+        res.end();
+    }
+
 })
 
 server.listen(4000);
