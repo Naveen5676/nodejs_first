875a6950d8d3df8835e3aa9cc93af7a930d23220
Naveen5676 <sample.first@yahoo.com>
2024-03-07 14:31:23 +0530

    Writing and reading from a file

---
+++ Getdata.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs= require('fs');
+
+const server  = http.createServer((req, res)=>{
+    const url = req.url
+    const method = req.method
+
+    if(url === '/'){
+        fs.readFile("message.txt" , {encoding: 'utf-8'}, (err, data)=>{
+            if(err){
+                console.log(err)
+            }
+            console.log('data from file'+data);
+            res.write('<html>');
+            res.write('<head><title>enter message</title></head>');
+            res.write(`<h2>${data}</h2>`)
+            res.write('<body><h2></h2><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        });
+    }
+    if(url === '/message' && method === 'POST'){
+        const body =[];
+        req.on( 'data', (chunk)=>{
+            console.log(chunk)
+            body.push(chunk);
+        });
+        return req.on('end',()=>{
+            const parsedbody = Buffer.concat(body).toString();
+            const message  = parsedbody.split('=')[1];
+            fs.writeFile('message.txt', message , (err)=>{
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        })
+    }
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<html>');
+    // res.write('<head><title>MY first page</title></head>');
+    // res.write('<body><h1> Hello from nodejs</h1></body>');
+    // res.write('</html>');
+    // res.end();
+})
+
+
+server.listen(4000)
