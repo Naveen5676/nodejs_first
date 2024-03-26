2938bce88077b4f836dc35f5d183adedb2ee6908
Naveen5676 <sample.first@yahoo.com>
2024-03-26 10:06:26 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const http = require("http");
+const path = require('path')
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
 
 const adminRoutes = require('./routes/admin') ; 
 const  shopRoutes=require('./routes/shop');
+const contactusRoutes = require('./routes/contactus');
 
 app.use(bodyParser.urlencoded({ extended: false })); //parser
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(contactusRoutes);
 
 
 app.use('/',(req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname , 'views', '404.html'))
 });
 
 // const server = http.createServer(app)
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const path = require('path')
+
+
+const rootDir = require('../util/path')
 
 const router = express.Router();
 
 //app.get is only fired only  for GET request and get does a exact match of they routes
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><label>Product name</label><input type="text" name="prodtitle"><br /><label>Product Size</label><input type="number" name="prodsize"><br /><button type="submit">Add Product</button></form>'
-  );
+ res.sendFile(path.join(rootDir , 'views', 'add-product.html'));
 });
 
 //we use app.post for incoming post request and app.use we use for all http request.
