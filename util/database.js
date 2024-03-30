a78c4b22950235f94d42c045d7902f0175976312
Naveen5676 <sample.first@yahoo.com>
2024-03-30 13:50:07 +0530

    Starting with MYSQL

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Wali@1065',
+});
+
+module.exports = pool.promise();
