0b3e4f2d79977a0632e7aed612e28a080fefe998
Naveen5676 <sample.first@yahoo.com>
2024-04-16 23:59:01 +0530

    Cart with many to many relationship

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cartitem");
 
 const app = express();
 
@@ -21,9 +23,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   User.findByPk(1)
-    .then(user=>{
+    .then((user) => {
       req.user = user; // makes the user object available to all templates
-      next();//so that we can move on with the next step 
+      next(); //so that we can move on with the next step
     })
     .catch((err) => console.log(err));
 });
@@ -35,6 +37,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" }); // cascade meaning if user is deleted  all products related to that user will be also deleted.
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem }); // in through we add an intermediate model which connects both and store they car and product
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   //.sync({force:true})//kepping force to true will update they table properties  if any changes found in the model
@@ -51,6 +57,9 @@ sequelize
   })
   .then((user) => {
     //console.log(user)
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
