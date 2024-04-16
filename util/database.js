bff55788123259e4010d70167b56e4fc98bc57b5
Naveen5676 <sample.first@yahoo.com>
2024-04-16 19:31:02 +0530

    Design the User and Product relationship

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("node-complete", "root", "Wali@1065", {
+const sequelize = new Sequelize("node-complete", "root", "Abcd@1234", {
   dialect: "mysql",
   host: "localhost",
 });
